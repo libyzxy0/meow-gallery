de0e892f20196e40f569f2af7348203c870e5e46
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2023-07-01 07:22:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let startTheShow = document.getElementById('start-btn');
+let gallerySection = document.getElementById('gallery');
+let heroSection = document.getElementById('hero');
+
+
+startTheShow.addEventListener('click', () => {
+    gallerySection.style.display="block";
+    window.location.href="#gallery";
+    setTimeout(() => {
+        heroSection.style.display="none";
+    }, 1000);
+    document.getElementById('music').play();
+    document.getElementById('nav').style.justifyContent="center";
+}) 
+
+
+let images = document.getElementById('images');
+
+function renderImage(url) {
+    let li = document.createElement('li');
+    let row = document.createElement('div');
+    row.className="row";
+    let image = document.createElement('img');
+    image.setAttribute("src", url)
+    images.appendChild(row);
+    row.appendChild(li);
+    li.appendChild(image);
+}
+
+
+for(let i = 0;i < 100;i++) {
+    fetch('https://cataas.com/cat?json=true')
+  .then(response => response.json())
+  .then(data => {
+  //console.log(`https://cataas.com${data.url}`)
+    // Process the returned data
+    renderImage(`https://cataas.com${data.url}`)
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the request
+    console.error('Error:', error);
+  });
+}
